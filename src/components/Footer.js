bb622f3d23f573b5b2fd3c89bb67d106a7476a13
damalga <dam.al.ga@gmx.com>
2020-07-23 20:42:46 +0200

    style deploy

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,15 +6,15 @@ class Footer extends React.Component{
     return (
       <footer>
           <section className="support">
-              <label>Pick ALCATAPE01 (Cassette) and support <a href="https://preciousplastic.com/">Precious Plastic</a>.</label>
+              <label>Pick ALCATAPE01 (Cassette) and support <a className="precious-plastic" href="https://preciousplastic.com/">Precious Plastic</a>.</label>
           </section>
           <section className="rrss">
             <ul className="rrss-list">
-              <li className="rs"><a href="https://soundcloud.com/alcarcia">Bandcamp</a></li>
-              <li className="rs"><a href="https://soundcloud.com/alcarcia">Discogs</a></li>
-              <li className="rs"><a href="https://soundcloud.com/alcarcia">Soundcloud</a></li>
-              <li className="rs"><a href="https://soundcloud.com/alcarcia">Facebook</a></li>
-              <li className="rs"><a href="https://soundcloud.com/alcarcia">Instagram</a></li>
+              <li className="rs"><a className="dg" href="https://www.discogs.com/artist/6426813-Alcarcia">Discogs</a></li>
+              <li className="rs"><a className="bc" href="https://alcarcia.bandcamp.com/releases">Bandcamp</a></li>
+              <li className="rs"><a className="sc" href="https://soundcloud.com/alcarcia">Soundcloud</a></li>
+              <li className="rs"><a className="fb" href="https://www.facebook.com/alcarcia/">Facebook</a></li>
+              <li className="rs"><a className="ig" href="https://www.instagram.com/alcarcia/">Instagram</a></li>
             </ul>
           </section>
       </footer>
--- src/components/Release.js
+++ src/components/Release.js
@@ -14,10 +14,16 @@ class Release extends React.Component{
     render () {
         return (
             <div className="release">
-                <img src={process.env.PUBLIC_URL + RELEASES_FOLDER + this.state.release.cover} alt="BoSsOb cover"></img>
+                <a href={this.state.release.linkRelease} target="_blank" rel="noopener noreferrer">
+                    <img src={process.env.PUBLIC_URL + RELEASES_FOLDER + this.state.release.cover} alt="BoSsOb cover"></img>
+                </a>
                 <div className="release-txt">
-                    <h3 className="release-title">{this.state.release.title}</h3>
-                    <h4 className="label">{this.state.release.label}</h4>
+                    <a href={this.state.release.linkRelease} className={this.state.release.linkColor} target="_blank" rel="noopener noreferrer">
+                        <h3 className="release-title">{this.state.release.title}</h3>
+                    </a>
+                    <a href={this.state.release.linkLabel} className={this.state.release.linkColor} target="_blank" rel="noopener noreferrer">
+                        <h4 className="label">{this.state.release.label}</h4>
+                    </a>
                     <h5 className="cat">{this.state.release.cat}</h5>
                     <h6 className="date">{this.state.release.date}</h6>
                     <ul className="tracklist">
