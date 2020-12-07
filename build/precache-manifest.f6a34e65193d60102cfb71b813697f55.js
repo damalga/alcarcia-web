e0f7d9d40334cc3674353ae07de48fcfe65927bc
damalga <dam.al.ga@gmx.com>
2020-12-07 22:16:29 +0100

    add build/ folder

---
+++ build/precache-manifest.f6a34e65193d60102cfb71b813697f55.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0de81391a0dd375dfbe08987c26b2ec",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b314e69943a506c0ca9",
+    "url": "/static/css/main.36080ba4.chunk.css"
+  },
+  {
+    "revision": "a04e56d836c8ceb3bd01",
+    "url": "/static/js/2.d6f6254e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d6f6254e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b314e69943a506c0ca9",
+    "url": "/static/js/main.d1f0ecca.chunk.js"
+  },
+  {
+    "revision": "7bb01e978037ec9f4394",
+    "url": "/static/js/runtime-main.31faeb47.js"
+  },
+  {
+    "revision": "09ad23519e9f3219baf1d5112d4761a0",
+    "url": "/static/media/antonio-bold-webfont.09ad2351.woff2"
+  },
+  {
+    "revision": "0b8b03f27a661230fa0a46dff02e760d",
+    "url": "/static/media/antonio-bold-webfont.0b8b03f2.woff"
+  },
+  {
+    "revision": "6cd173b9203009899d93cd7d2a2760c4",
+    "url": "/static/media/antonio-light-webfont.6cd173b9.woff2"
+  },
+  {
+    "revision": "cd6383cde1be8b9c5df7f7c2f1e4de9c",
+    "url": "/static/media/antonio-light-webfont.cd6383cd.woff"
+  },
+  {
+    "revision": "853186f210d8efd019ff1d433f54bba1",
+    "url": "/static/media/antonio-regular-webfont.853186f2.woff"
+  },
+  {
+    "revision": "f09bddddd60aae0063ce563c06e72a06",
+    "url": "/static/media/antonio-regular-webfont.f09bdddd.woff2"
+  }
+]);
