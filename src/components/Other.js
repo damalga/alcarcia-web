af999250c13fe73fd1fb4869fe53dd00d6742f9d
damalga <dam.al.ga@gmx.com>
2022-12-23 11:07:57 +0100

    checkpoint 23120222

--- src/components/Other.js
+++ src/components/Other.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import './../assets/stylesheets/css/style.css';
 
 class Others extends React.Component{
   render(){
     return (
         <section>
+          <div className='others-txt'>
           <h4 className="others-title">OTHERS</h4>
           <ul className="others-list">
             <li className="other"><a href="https://soundcloud.com/alcarcia/sets/mixes" target="_blank" rel="noopener noreferrer">Mixes</a></li>
-            <li className="other"><a href="https://unsolvedlabel.bandcamp.com/track/fluotexin-original-mix" target="_blank" rel="noopener noreferrer">Fluotexin (Original Mix) - </a></li>
+            <li className="other"><a href="https://unsolvedlabel.bandcamp.com/track/fluotexin-original-mix" target="_blank" rel="noopener noreferrer">Fluotexin (Original Mix)</a></li>
             <li className="other"><a href="https://nawalrecords.bandcamp.com/track/all-about" target="_blank" rel="noopener noreferrer">All About</a></li>
           </ul>
+          </div>
+          <img src={process.env.PUBLIC_URL + '/img/aside.png'} alt="Alcarcia Motor Bike"></img>
         </section>
     );
   }
