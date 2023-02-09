f54ab033f9eea78bd9bee626c56003a7aba48814
damalga <dam.al.ga@gmx.com>
2023-02-09 19:11:06 +0100

    add graphic stamp

--- src/components/Other.js
+++ src/components/Other.js
@@ -3,17 +3,20 @@ import React from 'react';
 class Others extends React.Component{
   render(){
     return (
-        <section>
+        <div>
+          <img className='bg-other' src={process.env.PUBLIC_URL + '/img/aside.png'} alt="Alcarcia Motor Bike"></img>
           <div className='others-txt'>
-          <h4 className="others-title">OTHERS</h4>
-          <ul className="others-list">
-            <li className="other"><a href="https://soundcloud.com/alcarcia/sets/mixes" target="_blank" rel="noopener noreferrer">Mixes</a></li>
-            <li className="other"><a href="https://unsolvedlabel.bandcamp.com/track/fluotexin-original-mix" target="_blank" rel="noopener noreferrer">Fluotexin</a></li>
-            <li className="other"><a href="https://nawalrecords.bandcamp.com/track/all-about" target="_blank" rel="noopener noreferrer">All About</a></li>
-          </ul>
+            <h4 className="others-title">OTHERS</h4>
+            <ul className="others-list">
+              <li className="other"><a href="https://soundcloud.com/alcarcia/sets/mixes" target="_blank" rel="noopener noreferrer">Mixes</a></li>
+              <li className="other"><a href="https://unsolvedlabel.bandcamp.com/track/fluotexin-original-mix" target="_blank" rel="noopener noreferrer">Fluotexin</a></li>
+              <li className="other"><a href="https://nawalrecords.bandcamp.com/track/all-about" target="_blank" rel="noopener noreferrer">All About</a></li>
+            </ul>
           </div>
-          <img src={process.env.PUBLIC_URL + '/img/aside.png'} alt="Alcarcia Motor Bike"></img>
-        </section>
+          <a className='download-cover-letter' href="/img/cover-letter.pdf" target="_blank">
+            <img src={process.env.PUBLIC_URL + '/img/download-cover-letter.svg'} alt="Download Cover Letter"></img>
+          </a>
+        </div>
     );
   }
 }
