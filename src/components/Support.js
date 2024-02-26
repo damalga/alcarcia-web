9402dd599a3e744a6724b7b778588b9a87b03d62
damalga <dam.al.ga@gmx.com>
2024-02-26 14:25:15 +0100

    fix support module

--- src/components/Support.js
+++ src/components/Support.js
@@ -6,7 +6,7 @@ class Support extends React.Component {
             <div className='support-container'>
                 <div className='thanks-container'>
                     <div className='thanks-txt'>
-                        <p>Thanks to your contribution buying <a href='https://alcatapes.bandcamp.com/' target='_blank'>ALCATAPES</a>, we've funded a 6-month subscription through <a href='https://www.patreon.com/one_army/' target='_blank'>Patreon</a> to <a href='https://www.onearmy.earth/' target='_blank'>One Army</a>, supporting their mission for a sustainable future. They are also part of amazing circular innovative ideas like Precious Plastic, Project Kamp, Fixing Fashion, among others. Thank you for being part of this impactful journey.</p>
+                        <p>Thanks to your contribution buying <a href='https://alcatapes.bandcamp.com/' target='_blank' rel="noopener noreferrer">ALCATAPES</a>, we've funded a 6-month subscription through <a href='https://www.patreon.com/one_army/' target='_blank' rel="noopener noreferrer">Patreon</a> to <a href='https://www.onearmy.earth/' target='_blank' rel="noopener noreferrer">One Army</a>, supporting their mission for a sustainable future. They are also part of amazing circular innovative ideas like Precious Plastic, Project Kamp, Fixing Fashion, among others. Thank you for being part of this impactful journey.</p>
                     </div>
                 </div>
                 <div className='damalga-container'>
