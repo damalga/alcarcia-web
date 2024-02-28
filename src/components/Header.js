7181fbd6313c7186635a8872aa18cb9dfeaabc15
damalga <dam.al.ga@gmx.com>
2024-02-28 19:18:15 +0100

    upload new pdf and few css changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,8 +14,8 @@ class Header extends React.Component{
         </section>
         <section className="section-2">
           <h2 className="releases-title">Releases</h2>
-          <label className="contact">For any enquiry:
-            <a href="mailto:alcarcia@gmx.com" className="alca-yellow">alcarciamusic@gmail.com</a>
+          <label className="contact">For any enquiry: 
+            <a href="mailto:alcarcia@gmx.com" className="alca-yellow"> alcarciamusic@gmail.com</a>
           </label>
         </section>
       </header>
