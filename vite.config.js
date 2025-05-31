7f71d5ac0358fbc33a716da2ec14dc33db55bd2c
damalga <damalga@protonmail.com>
2025-05-31 21:21:12 +0200

    build/dist

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,5 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
-  // Si usas una estructura con "src", puedes añadir:
-  root: 'src',
-  publicDir: '../public', // O ajusta si cambias tu estructura
+  plugins: [react()]
 });
