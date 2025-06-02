fda4fd4fdeb1f1531f903462863e8c242bb4882c
damalga <dam.al.ga@gmx.com>
2025-06-02 21:30:36 +0200

    update correctly cover letter

--- src/components/Other.jsx
+++ src/components/Other.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 
 import asideImg from '../assets/img/aside.png';
 import downloadCoverLetter from '../assets/img/download-cover-letter.svg';
+import coverLetter from '../assets/img/alcarcia-cover-letter.png';
 
 class Others extends React.Component{
   render(){
     return (
         <div>
-          <a className='download-cover-letter' href="/img/alcarcia-cover-letter.png" target="_blank">
+          <a className='download-cover-letter' href={coverLetter} target="_blank">
             <img src={downloadCoverLetter} alt="Download Cover Letter"></img>
           </a>
           <img src={asideImg} className='bg-other' alt="Alcarcia Motor Bike"></img>
