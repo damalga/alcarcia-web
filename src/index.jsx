ddba2b06d4447fa22edc9743f2e91ae30ffbe899
damalga <damalga@protonmail.com>
2026-05-20 22:00:09 +0200

    add lenix smoothscroll

--- src/index.jsx
+++ src/index.jsx
@@ -1,15 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import Lenis from 'lenis';
 import Header from './components/Header';
 import Main from './components/Main';
 import Footer from './components/Footer';
 
 import './assets/stylesheets/base.scss';
 
+function App() {
+  useEffect(() => {
+    const lenis = new Lenis({ lerp: 0.1 });
+    let rafId;
+    function raf(time) {
+      lenis.raf(time);
+      rafId = requestAnimationFrame(raf);
+    }
+    rafId = requestAnimationFrame(raf);
+
+    return () => {
+      cancelAnimationFrame(rafId);
+      lenis.destroy();
+    };
+  }, []);
+
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
+  );
+}
+
 ReactDOM.createRoot(document.getElementById('alcarcia')).render(
   <React.StrictMode>
-    <Header />
-    <Main />
-    <Footer />
+    <App />
   </React.StrictMode>
 );
